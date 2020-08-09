0af7cd5e14ed91199f6a8a310ef83870a9a2d0a6
Karim Jouar aka jargoman <karim@live.ca>
2020-08-09 15:09:13 -0400

    Add files via upload

--- skin/skin.py
+++ skin/skin.py
@@ -4,37 +4,32 @@ import sys
 import hashlib
 import base58
 
-if (len(sys.argv) != 4):
-	print('usage : python skin.py {encrypt,decrypt} blob key')
+if (len(sys.argv) != 3):
+	print('usage : python skin.py blob key')
 	sys.exit('incorrect usage')
 
-alphabet='rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz'
 
-letters='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKL'
-
-command=sys.argv[1]
-
-blob=sys.argv[2]
+blob=sys.argv[1]
 
 
 
 
-key=sys.argv[3].encode();
+key=sys.argv[2].encode();
 
 salt=b'dontbesalty91726'
 
-print('command : ', command)
+
 print('blob : ', blob)
 print('key', key)
 
 dk = hashlib.pbkdf2_hmac('sha256', key, salt, 100000, dklen=21)
 
-print(len(dk))
+#print(len(dk))
 
 
 decoded = base58.b58decode(blob, alphabet=base58.RIPPLE_ALPHABET)
 
-print(len(decoded))
+#print(len(decoded))
 
 xored = bytes(a ^ b for (a, b) in zip(decoded, dk))
 
